e3bd882fbab23e0896ab2f26a3979f43e1574cbc
mustafatalha222 <mustafatalha222@gmail.com>
2023-01-15 14:23:19 +0500

    ui view project done

--- src/pages/Dashboard/ProjectDetails.js
+++ src/pages/Dashboard/ProjectDetails.js
@@ -14,12 +14,15 @@ import {
   Box,
   Stack,
   Skeleton,
+  Avatar,
+  Chip,
 } from '@mui/material';
 // components
 import Page from '../../components/Page';
 // sections
 import { getProjectDetails } from 'supabase/projects';
 import Iconify from 'components/Iconify';
+import { useTheme } from '@mui/material/styles';
 
 // ----------------------------------------------------------------------
 
@@ -58,33 +61,50 @@ export default function Projects() {
             Something went wrong!
           </Alert>
         </Snackbar>
+
+        {/* <Grid item xs={12}>
+          {loading ? (
+            <>
+              <Skeleton />
+              <Skeleton animation="wave" />
+              <Skeleton animation={false} />
+            </>
+          ) : (
+            <Card>
+              <CardHeader title={data?.title} />
+              <CardContent>
+                <Typography variant="body2">Contract code: {data?.contract_code}</Typography>
+                <Typography variant="body2">Contract value: {data?.contract_value}</Typography>
+                <Typography variant="body2">
+                  Project timeline: {data?.start} - {data?.end}
+                </Typography>
+              </CardContent>
+            </Card>
+          )}
+        </Grid> */}
+
         <Grid container spacing={3}>
-          <Grid item xs={12}>
-            {loading ? (
-              <>
-                <Skeleton />
-                <Skeleton animation="wave" />
-                <Skeleton animation={false} />
-              </>
-            ) : (
-              <Card>
-                <CardHeader title={data?.title} />
-                <CardContent>
-                  <Typography variant="body2">Contract code: {data?.contract_code}</Typography>
-                  <Typography variant="body2">Contract value: {data?.contract_value}</Typography>
-                  <Typography variant="body2">
-                    Project timeline: {data?.start} - {data?.end}
-                  </Typography>
-                </CardContent>
-              </Card>
-            )}
+          {eventCost.map((event, index) => (
+            <>
+              <Grid item xs={12} sm={6} md={4} lg={2}>
+                <EventCardCost event={event} key={index} />
+              </Grid>
+            </>
+          ))}
+
+          <Grid item xs={12} sm={4} md={3}>
+            <ProjectInfo projectInfo={projectInfo} />
           </Grid>
+          <Grid item xs={12} sm={8} md={9}>
+            <TeamMates projectInfo={projectInfo} title={data?.title} />
+          </Grid>
+
           {loading ? (
             <Loading />
           ) : (
             events.map((event, index) => (
               <>
-                <Grid item xs={12} md={6} lg={4}>
+                <Grid item xs={12} sm={6} md={4} lg={2}>
                   <EventCard event={event} key={index} />
                 </Grid>
               </>
@@ -121,17 +141,48 @@ function EventCard({ event, key }) {
           flexDirection: 'column',
           borderRadius: 2,
           cursor: 'pointer',
+          height: '100%',
         }}
         align="center"
         p={2}
         onClick={redirectCard}
+        maxHeight={'sm'}
       >
         {event?.icon}
-        <Typography m={1} align="center" variant="h6">
+        <Typography m={1} align="center" sx={{ fontWeight: 600 }}>
           {event?.title}
         </Typography>
         <Typography align="center" variant="caption">
           {event?.description}
+        </Typography>
+      </Box>
+    </>
+  );
+}
+
+function EventCardCost({ event, key }) {
+  return (
+    <>
+      <Box
+        key={key}
+        sx={{
+          backgroundColor: (theme) => theme.palette.background.paper,
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          borderRadius: 2,
+          cursor: 'pointer',
+          height: '100%',
+        }}
+        align="center"
+        p={2}
+        maxHeight={'sm'}
+      >
+        <Typography m={1} align="center" sx={{ fontWeight: 600, fontSize: 14 }}>
+          {event?.title}
+        </Typography>
+        <Typography align="center" variant="caption" color={event?.color} fontSize={16}>
+          {event?.cost}
         </Typography>
       </Box>
     </>
@@ -160,6 +211,96 @@ function Loading() {
         <Skeleton variant="rectangular" width={210} height={20} />
       </Stack>
     </Grid>
+  );
+}
+
+function ProjectInfo({ projectInfo }) {
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: (theme) => theme.palette.background.paper,
+          borderRadius: 2,
+          height: '100%',
+        }}
+        p={2}
+      >
+        <Typography m={1} sx={{ fontWeight: 600, fontSize: 16 }}>
+          Project info
+        </Typography>
+        {projectInfo?.map((e) => (
+          <Grid container spacing={2} style={{ alignItems: 'center' }} mt={1} pl={3}>
+            <Iconify width={15} height={15} icon={e.icon} />
+            <Typography m={1} sx={{ fontSize: 12, color: 'text.secondary' }}>
+              {e.title}
+            </Typography>
+          </Grid>
+        ))}
+      </Box>
+    </>
+  );
+}
+
+function TeamMates({ projectInfo, title }) {
+  const theme = useTheme();
+  const randomPick = () => {
+    const colors = ['#FF62B5', '#8CCC67', '#8D99FF'];
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
+
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: (theme) => theme.palette.background.paper,
+          borderRadius: 2,
+        }}
+        p={2}
+      >
+        <Typography m={1} sx={{ fontWeight: 600, fontSize: 16 }}>
+          TeamMates
+        </Typography>
+        <Grid container spacing={4}>
+          {Array.from(Array(16).keys())?.map((e) => {
+            return (
+              <Grid item xs={12} sm={6} md={5} lg={3} style={{ flexDirection: 'row' }} mt={1} pl={3}>
+                <Box sx={{ display: 'flex' }}>
+                  <Box pt={0.3}>
+                    <Avatar sx={{ bgcolor: randomPick, width: 18, height: 18, fontSize: 12 }}>E</Avatar>
+                  </Box>
+                  <Box pl={1}>
+                    <Box justifyContent={'space-between'} display="flex">
+                      <Typography variant="caption" color={theme.palette.text.secondary}>
+                        Employee_{(Math.random() + 1).toString(36).substring(9)}
+                      </Typography>
+                      {e % 12 === 0 && (
+                        <Typography
+                          style={{
+                            borderRadius: 3,
+                            background: '#FF6B00',
+                            color: 'white',
+                            fontSize: 8,
+                            height: 15,
+                            paddingInline: 5,
+                            paddingBlock: 1,
+                          }}
+                          variant={'caption'}
+                        >
+                          TEAM LEAD
+                        </Typography>
+                      )}
+                    </Box>
+                    <Box>
+                      <Typography variant="caption">{title}</Typography>
+                    </Box>
+                  </Box>
+                </Box>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Box>
+    </>
   );
 }
 
@@ -199,5 +340,69 @@ const events = [
     title: 'Profit & Loss Reports',
     description: 'Pellentesque in ipsum id orci porta dapibus',
     redirect: '#',
+  },
+];
+
+const eventCost = [
+  {
+    title: 'Fixed Cost',
+    cost: '1,278,663,306',
+    color: '#8D99FF',
+  },
+  {
+    title: 'Labor Cost',
+    cost: '1,200,629,509',
+    color: '#FFA58D',
+  },
+  {
+    title: 'Administrative Expenses',
+    cost: '42,033,797',
+    color: '#98D2C3',
+  },
+  {
+    title: 'Asset Maintenance Cost',
+    cost: '36,000,000',
+    color: '#8CCC67',
+  },
+  {
+    title: 'Operating Profit',
+    cost: '-143,207,384',
+    color: '#7FBCFE',
+  },
+  {
+    title: 'Operating Profit (%)',
+    cost: '-4%',
+    color: '#FF62B5',
+  },
+];
+
+const projectInfo = [
+  {
+    title: 'Private Co',
+    icon: 'material-symbols:handshake-outline-sharp',
+  },
+  {
+    title: 'Full Service',
+    icon: 'material-symbols:home-repair-service-outline',
+  },
+  {
+    title: '154 kV',
+    icon: 'radix-icons:lightning-bolt',
+  },
+  {
+    title: 'Jinju-Hadong Underground',
+    icon: 'heroicons-outline:location-marker',
+  },
+  {
+    title: 'Iljin Electric',
+    icon: 'bi:buildings',
+  },
+  {
+    title: '43993',
+    icon: 'ion:document-text-outline',
+  },
+  {
+    title: '169,000,000',
+    icon: 'material-symbols:account-balance-wallet-outline',
   },
 ];
