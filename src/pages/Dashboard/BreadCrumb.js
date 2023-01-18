2b83d130e35d29b2c58a62354adcd14c348b3c59
mustafatalha222 <mustafatalha222@gmail.com>
2023-01-18 23:30:43 +0500

    project api details show from db

--- src/pages/Dashboard/BreadCrumb.js
+++ src/pages/Dashboard/BreadCrumb.js
@@ -1,155 +1,155 @@
-import * as React from 'react';
-import { Breadcrumbs, Link, Stack, Menu, MenuItem, Typography, Button as MuiButton } from '@mui/material';
-import { useNavigate, useLocation } from 'react-router-dom';
-import _ from 'lodash';
-import { styled, alpha } from '@mui/material/styles';
-import { KeyboardArrowDown as KeyboardArrowDownIcon } from '@mui/icons-material';
-import Iconify from 'components/Iconify';
-import useMain from 'pages/context/context';
-import { MainActionType } from 'pages/context/types';
+import * as React from 'react'
+import { Breadcrumbs, Link, Stack, Menu, MenuItem, Typography, Button as MuiButton } from '@mui/material'
+import { useNavigate, useLocation } from 'react-router-dom'
+import _ from 'lodash'
+import { styled, alpha } from '@mui/material/styles'
+import { KeyboardArrowDown as KeyboardArrowDownIcon } from '@mui/icons-material'
+import Iconify from 'components/Iconify'
+import useMain from 'pages/context/context'
+import { MainActionType } from 'pages/context/types'
 
 export default function CustomSeparator(props) {
-  const { state, dispatch } = useMain();
-  const { isfilterOpen } = state.filters || {};
+	const { state, dispatch } = useMain()
+	const { isfilterOpen } = state.filters || {}
 
-  const { selected } = props;
-  const history = useNavigate();
-  const { pathname } = useLocation();
-  const pathnames = pathname.split('/').filter((x) => x);
-  const basePath = `/${pathnames.slice(0, 2).join('/')}`;
-  pathnames.splice(0, 2);
+	const { selected } = props
+	const history = useNavigate()
+	const { pathname } = useLocation()
+	const pathnames = pathname.split('/').filter((x) => x)
+	const basePath = `/${pathnames.slice(0, 2).join('/')}`
+	pathnames.splice(0, 2)
 
-  const openFilter = () => {
-    dispatch({ type: MainActionType.CHANGE_FILTER, bool: !isfilterOpen });
-  };
+	const openFilter = () => {
+		dispatch({ type: MainActionType.CHANGE_FILTER, bool: !isfilterOpen })
+	}
 
-  return (
-    <Stack padding={1} spacing={2}>
-      <Breadcrumbs separator="›" aria-label="breadcrumb">
-        <MuiButton
-          size="small"
-          variant="contained"
-          color={`${isfilterOpen ? 'secondary' : 'inherit'}`}
-          sx={{ padding: 1, minWidth: 0, width: 35 }}
-          onClick={openFilter}
-        >
-          <Iconify icon="heroicons-funnel" width={20} height={20} />
-        </MuiButton>
-        {pathnames.length > 0 ? <Link onClick={() => history(basePath)}>Main</Link> : <Typography> Main </Typography>}
-        {pathnames.map((name, index) => {
-          const routeTo = `${basePath}+/${pathnames.slice(0, index + 1).join('/')}`;
-          const isLast = index === pathnames.length - 1;
-          if (index === 0) return <CustomizedMenus key={2} option={name} />;
-          if (isLast)
-            return (
-              <Typography fontWeight="600" key={name}>
-                {_.startCase(name)}
-              </Typography>
-            );
-          return (
-            <Link key={name} onClick={() => history(routeTo)}>
-              {name}
-            </Link>
-          );
-        })}
-      </Breadcrumbs>
-    </Stack>
-  );
+	return (
+		<Stack padding={1} spacing={2}>
+			<Breadcrumbs separator="›" aria-label="breadcrumb">
+				<MuiButton
+					size="small"
+					variant="contained"
+					color={`${isfilterOpen ? 'secondary' : 'inherit'}`}
+					sx={{ padding: 1, minWidth: 0, width: 35 }}
+					onClick={openFilter}
+				>
+					<Iconify icon="heroicons-funnel" width={20} height={20} />
+				</MuiButton>
+				{pathnames.length > 0 ? <Link onClick={() => history(basePath)}>Main</Link> : <Typography> Main </Typography>}
+				{pathnames.map((name, index) => {
+					const routeTo = `${basePath}+/${pathnames.slice(0, index + 1).join('/')}`
+					const isLast = index === pathnames.length - 1
+					if (index === 0) return <CustomizedMenus key={2} option={name} />
+					if (isLast)
+						return (
+							<Typography fontWeight="600" key={name}>
+								{_.startCase(name)}
+							</Typography>
+						)
+					return (
+						<Link key={name} onClick={() => history(routeTo)}>
+							{name}
+						</Link>
+					)
+				})}
+			</Breadcrumbs>
+		</Stack>
+	)
 }
 
 const titles = (name) => {
-  switch (name) {
-    case 'add':
-      return 'Add New Project';
-    case 'list':
-      return 'Projects';
-    default:
-      return 'View Project';
-  }
-};
+	switch (name) {
+		case 'add':
+			return 'Add New Project'
+		case 'list':
+			return 'Projects'
+		default:
+			return 'View Project'
+	}
+}
 
 const StyledMenu = styled((props) => (
-  <Menu
-    elevation={0}
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'right',
-    }}
-    transformOrigin={{
-      vertical: 'top',
-      horizontal: 'right',
-    }}
-    {...props}
-  />
+	<Menu
+		elevation={0}
+		anchorOrigin={{
+			vertical: 'bottom',
+			horizontal: 'right',
+		}}
+		transformOrigin={{
+			vertical: 'top',
+			horizontal: 'right',
+		}}
+		{...props}
+	/>
 ))(({ theme }) => ({
-  '& .MuiPaper-root': {
-    borderRadius: 6,
-    marginTop: theme.spacing(1),
-    minWidth: 180,
-    color: theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
-    boxShadow:
-      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-    '& .MuiMenu-list': {
-      padding: '4px 0',
-    },
-    '& .MuiMenuItem-root': {
-      '& .MuiSvgIcon-root': {
-        fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5),
-      },
-      '&:active': {
-        backgroundColor: alpha(theme.palette.primary.main, theme.palette.action.selectedOpacity),
-      },
-    },
-  },
-}));
+	'& .MuiPaper-root': {
+		borderRadius: 6,
+		marginTop: theme.spacing(1),
+		minWidth: 180,
+		color: theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+		boxShadow:
+			'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+		'& .MuiMenu-list': {
+			padding: '4px 0',
+		},
+		'& .MuiMenuItem-root': {
+			'& .MuiSvgIcon-root': {
+				fontSize: 18,
+				color: theme.palette.text.secondary,
+				marginRight: theme.spacing(1.5),
+			},
+			'&:active': {
+				backgroundColor: alpha(theme.palette.primary.main, theme.palette.action.selectedOpacity),
+			},
+		},
+	},
+}))
 
 function CustomizedMenus({ option }) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+	const [anchorEl, setAnchorEl] = React.useState(null)
+	const open = Boolean(anchorEl)
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget)
+	}
+	const handleClose = () => {
+		setAnchorEl(null)
+	}
 
-  return (
-    <div>
-      <Typography
-        display="flex"
-        direction="row"
-        alignItems="center"
-        key="3"
-        color="text.primary"
-        label="Accessories"
-        onClick={handleClick}
-      >
-        {titles(option)}
-        <KeyboardArrowDownIcon />
-      </Typography>
+	return (
+		<div>
+			<Typography
+				display="flex"
+				direction="row"
+				alignItems="center"
+				key="3"
+				color="text.primary"
+				label="Accessories"
+				onClick={handleClick}
+			>
+				{titles(option)}
+				<KeyboardArrowDownIcon />
+			</Typography>
 
-      <StyledMenu
-        id="demo-customized-menu"
-        MenuListProps={{
-          'aria-labelledby': 'demo-customized-button',
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-      >
-        <MenuItem selected={option === 'add'} onClick={handleClose} disableRipple>
-          <Link underline="hover" key="11" color="inherit" href="/dashboard/projects/add">
-            Add new project
-          </Link>
-        </MenuItem>
-        <MenuItem selected={option === 'list'} onClick={handleClose} disableRipple>
-          <Link underline="hover" key="12" color="inherit" href="/dashboard/projects/list">
-            Projects list
-          </Link>
-        </MenuItem>
-      </StyledMenu>
-    </div>
-  );
+			<StyledMenu
+				id="demo-customized-menu"
+				MenuListProps={{
+					'aria-labelledby': 'demo-customized-button',
+				}}
+				anchorEl={anchorEl}
+				open={open}
+				onClose={handleClose}
+			>
+				<MenuItem selected={option === 'add'} onClick={handleClose} disableRipple>
+					<Link underline="hover" key="11" color="inherit" href="/dashboard/projects/add">
+						Add new project
+					</Link>
+				</MenuItem>
+				<MenuItem selected={option === 'list'} onClick={handleClose} disableRipple>
+					<Link underline="hover" key="12" color="inherit" href="/dashboard/projects/list">
+						Projects list
+					</Link>
+				</MenuItem>
+			</StyledMenu>
+		</div>
+	)
 }
--- src/supabase/employees.js
+++ src/supabase/employees.js
@@ -1,6 +1,11 @@
-import { supabase } from 'lib/api';
+import { supabase } from 'lib/api'
 
 export const listAllEmployees = async () => {
-  const res = await supabase.from('employees').select('*');
-  return res;
-};
+	const res = await supabase.from('employees').select('*')
+	return res
+}
+
+export const listEmployeesByProject = async (projectId) => {
+	const res = await supabase.from('employees').select('*').eq('project', projectId)
+	return res
+}
