22127ca3323831a25f8c4528d4874a05fb1242f2
Sweekar <sweekar07@yahoo.com>
2023-03-08 14:58:57 +0530

    collapased teams on te page load for demo

--- src/pages/Dashboard/TravelExpenses/Timeline.js
+++ src/pages/Dashboard/TravelExpenses/Timeline.js
@@ -209,6 +209,7 @@ export default function Timeline() {
 				employees = [...employees, ...teamEmployees]
 				return {
 					...team,
+					collapsed: true,
 					children: updatedEmployees,
 				}
 			}),
