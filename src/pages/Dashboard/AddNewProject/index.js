a9dfd19b73735c56d467b561f7fa4c9fe00168dc
Divyansh Kumar <divyanshkumar@Divyanshs-MacBook-Air.local>
2024-05-27 20:17:00 +0530

    added new diagram builder tab

--- src/pages/Dashboard/AddNewProject/index.js
+++ src/pages/Dashboard/AddNewProject/index.js
@@ -1,126 +1,97 @@
-import '@mobiscroll/react/dist/css/mobiscroll.min.css'
-import 'ag-grid-community/styles/ag-grid.css'
-import 'ag-grid-community/styles/ag-theme-alpine.css'
-import 'ag-theme-ddconst.scss'
-
-import {
-	AccordionDetails,
-	Accordion as MuiAccordion,
-	AccordionSummary as MuiAccordionSummary,
-	Stack,
-} from '@mui/material'
-import { styled } from '@mui/material/styles'
+import { useState } from 'react'
+import { Tabs, Tab, Stack, Box } from '@mui/material'
 import Iconify from 'components/Iconify'
 import PropTypes from 'prop-types'
-
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom'
 import Contract from './Contract'
 import ExecutionBudget from './ExcecutionBudget'
 import Tasks from './Tasks/Tasks'
 import FlowDiagram from 'pages/WeeklyPlan/FlowDiagram'
+import { styled } from '@mui/material/styles'
 
-const Accordion = styled((props) => <MuiAccordion disableGutters elevation={0} square {...props} />)(() => ({
-	background: 'transparent',
-	border: '1px solid rgba(0, 0, 0, 0.1)',
-	overflow: 'hidden',
-	borderRadius: '8px',
-	'&:not(:last-child)': {
-		borderBottom: 0,
-	},
-	'&:before': {
-		display: 'none',
-	},
-}))
-
-const AccordionSummary = styled((props) => (
-	<MuiAccordionSummary
-		expandIcon={<Iconify icon="material-symbols:arrow-forward-ios-rounded" width={15} height={15} />}
-		{...props}
-	/>
-))(({ theme }) => ({
-	backgroundColor: theme.palette.background.paper,
-
-	borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
-	'& .Mui-expanded': {
-		borderTopLeftRadius: '8px',
-		borderTopRightRadius: '8px',
-	},
-}))
+const StyledBox = styled(Box)({
+	padding: '8px 16px 16px',
+})
 
 const AddNewProject = ({ edit = false }) => {
 	const [searchParams] = useSearchParams()
-	const { pathname } = useLocation()
+	const { pathname } = useLocation() 
 	const navigate = useNavigate()
 
 	const tab = searchParams.get('tab')
+	const [selectedTab, setSelectedTab] = useState(tab || '1')
 
-	const handleChange = (id) => (event, isExpanded) => {
+	const handleChange = (event, newTab) => {
+		setSelectedTab(newTab)
 		navigate({
 			pathname,
-			search: isExpanded ? `?tab=${id}` : '',
+			search: newTab !== '1' ? `?tab=${newTab}` : '',
 		})
 	}
+
 	return (
-		<div>
-			<Accordion expanded={tab === '1'} onChange={handleChange(1)}>
-				<AccordionSummary
-					expandIcon={<Iconify icon="material-symbols:expand-more-rounded" width={20} height={20} />}
-					aria-controls="panel1a-content"
-					id="panel1a-header"
-				>
-					<Stack gap={2} direction="row" alignItems="center">
-						{edit ? (
-							<Iconify color="#8CCC67" icon="material-symbols:check-circle-outline-rounded" width={20} height={20} />
-						) : (
-							<img style={{ height: 15 }} src="/static/images/contract.svg" alt="" />
-						)}
-						Contract / Project Information
-					</Stack>
-				</AccordionSummary>
-				<AccordionDetails>
-					<Contract edit={edit} />
-				</AccordionDetails>
-			</Accordion>
-			<Accordion disabled={!edit} expanded={tab === '2'} onChange={handleChange(2)}>
-				<AccordionSummary
-					expandIcon={<Iconify icon="material-symbols:expand-more-rounded" width={20} height={20} />}
-					aria-controls="panel2a-content"
-					id="panel2a-header"
-				>
-					<Stack gap={2} direction="row" alignItems="center">
-						<Iconify sx={{ color: '#FF6B00' }} icon="tabler:clipboard-list" width={20} height={20} /> Tasks
-					</Stack>
-				</AccordionSummary>
-				<AccordionDetails>{edit && tab === '2' && <Tasks />}</AccordionDetails>
-			</Accordion>
-			<Accordion disabled={!edit} expanded={tab === '3'} onChange={handleChange(3)}>
-				<AccordionSummary
-					expandIcon={<Iconify icon="material-symbols:expand-more-rounded" width={20} height={20} />}
-					aria-controls="panel2a-content"
-					id="panel2a-header"
-				>
-					<Stack gap={2} direction="row" alignItems="center">
-						<Iconify sx={{ color: '#FF6B00' }} icon="heroicons-outline:calculator" width={20} height={20} /> Execution
-						Budget
-					</Stack>
-				</AccordionSummary>
-				<AccordionDetails sx={{ background: (theme) => theme.palette.background.default }}>
-					{edit && tab === '3' && <ExecutionBudget />}
-				</AccordionDetails>
-			</Accordion>
-			<Accordion expanded={tab === '4'} onChange={handleChange(4)}>
-				<AccordionSummary
-					expandIcon={<Iconify icon="material-symbols:expand-more-rounded" width={20} height={20} />}
-					aria-controls="panel2a-content"
-					id="panel2a-header"
-				>
-					<Stack gap={2} direction="row" alignItems="center">
-						<Iconify sx={{ color: '#FF6B00' }} icon="raphael:diagram" width={20} height={20} /> Diagram Builder
-					</Stack>
-				</AccordionSummary>
-				<AccordionDetails>{edit && tab === '4' && <FlowDiagram isEditable={true} />}</AccordionDetails>
-			</Accordion>
-		</div>
+		<Box border={1} borderColor={'lightgrey'} borderRadius={1} marginLeft={4} marginRight={4} sx={{ overflow: "hidden"}}>
+			<Tabs
+				value={selectedTab}
+				onChange={handleChange}
+				variant="fullWidth"
+				textColor='inherit'
+				sx={{ bgcolor: 'white'}}
+				TabIndicatorProps={{ hidden: true }}
+				
+			>
+				<Tab
+					value="1"
+					label={
+						<Stack gap={2} direction="row" alignItems="center" sx={{ fontFamily: 'manrope', fontWeight: 600 }}>
+							{edit ? (
+								<Iconify color={selectedTab === '1' ? "#FF6B00" : undefined} icon="ic:outline-design-services" width={20} height={20} />
+							) : (
+								<img style={{ height: 15 }} src="/static/images/contract.svg" alt="" />
+							)}
+							Contract / Project Information
+						</Stack>
+					}
+					sx={{  borderRight: 1, borderColor: 'divider' }}
+				/>
+				<Tab
+					value="2"
+					label={
+						<Stack gap={2} direction="row" alignItems="center" sx={{ fontFamily: 'manrope', fontWeight: 600 }}>
+							<Iconify color={selectedTab === '2' ? '#FF6B00' : undefined} icon="tabler:clipboard-list" width={20} height={20} /> Tasks
+						</Stack>
+					}
+					disabled={!edit}
+					sx={{  borderRight: 1, borderColor: 'divider' }}
+				/>
+				<Tab
+					value="3"
+					label={
+						<Stack gap={2} direction="row" alignItems="center" sx={{ fontFamily: 'manrope', fontWeight: 600 }}>
+							<Iconify color={selectedTab === '3' ? '#FF6B00' : undefined} icon="heroicons-outline:calculator" width={20} height={20} /> Execution Budget
+						</Stack>
+					}
+					disabled={!edit}
+					sx={{  borderRight: 1, borderColor: 'divider' }}
+				/>
+				<Tab
+					value="4"
+					label={
+						<Stack gap={2} direction="row" alignItems="center" sx={{ fontFamily: 'manrope', fontWeight: 600 }}>
+							<Iconify color={selectedTab === '4' ? '#FF6B00' : undefined} icon="raphael:diagram" width={20} height={20} /> Diagram Builder
+						</Stack>
+					}
+					disabled={!edit}
+					sx={{  borderRight: 1, borderColor: 'divider' }}
+				/>
+			</Tabs>
+			<Box>
+				{selectedTab === '1' && (<StyledBox ><Contract edit={edit} /></StyledBox>)}
+				{selectedTab === '2' && edit && (<StyledBox ><Tasks /></StyledBox>)}
+				{selectedTab === '3' && edit && (<StyledBox ><ExecutionBudget /></StyledBox>)}
+				{selectedTab === '4' && edit && (<StyledBox ><FlowDiagram isEditable={true} /></StyledBox>)}
+			</Box>
+		</Box>
 	)
 }
 
